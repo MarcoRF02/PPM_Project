4a181b1b0152f40620cff6e8a77b4912d53c3a0d
MarcoRF02 <marco.rf2002@gmail.com>
2024-05-18 21:22:37 +0200

    Started to implement home page layout.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// js/scripts.js
+
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-100%';
+    } else {
+        sidebar.style.left = '0';
+    }
+}
